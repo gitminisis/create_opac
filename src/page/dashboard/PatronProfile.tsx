7fed3d3cb5a6ea2e49659e3eb8f42d997c56960c
donRyu0125 <don@minisisinc.com>
2025-09-05 16:03:18 -0400

    cams 1 change

--- src/page/dashboard/PatronProfile.tsx
+++ src/page/dashboard/PatronProfile.tsx
@@ -24,10 +24,11 @@ import {
 import { colorClasses } from './constants'
 
 export interface StatCardProps {
-	icon: React.ReactNode
-	label: string
-	color: string
-	value: any
+	icon?: React.ReactNode
+	label?: string
+	color?: string
+	value?: any
+	ref?: any
 }
 
 export default function PatronProfile() {
@@ -35,7 +36,6 @@ export default function PatronProfile() {
 	const { home, archives, museum, library, message, clientProfile } = useConstants()
 	const profileList = clientProfile.database
 	const m2l_patron_id = getCookieValue('M2L_PATRON_ID')?.split(']')[1]
-
 
 	const clientDashboardCards = [
 		{
@@ -107,23 +107,23 @@ export default function PatronProfile() {
 
 	return (
 		<PatronLayout
-			
 			mainHeading={
 				<>
 					<Home className="mr-1 h-5 w-5" />
 					<h2 className="text-lg font-semibold text-gray-900">{message.clientDashboard}</h2>
 				</>
-			}>
-			<div className="mb-4 rounded-md bg-white p-6 shadow">
+			}
+			isLibrary={false}>
+			{/* <div className="mb-4 rounded-md bg-white p-6 shadow">
 				<h1 className="text-3xl font-semibold text-gray-800">
 					{message.welcome} {records[0]?.full_name || 'User'}!
 				</h1>
 				<p className="mt-2">{message.welcomeMessage}</p>
-			</div>
+			</div> */}
 
 			{/* Stats Grid */}
 			<div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-				{clientDashboardCards.map((card, index) => {
+				{/* {clientDashboardCards.map((card, index) => {
 					const isSpecialLabel = card.label === 'Bookmarks' || card.label === 'Library Portal'
 					const href = getCookieValue('HOME_SESSID') + card.link + (isSpecialLabel ? '' : m2l_patron_id)
 
@@ -132,9 +132,9 @@ export default function PatronProfile() {
 							<StatCard icon={card.icon} label={card.label} color={card.color} value={card.value} />
 						</a>
 					)
-				})}
+				})} */}
 				{/* Calednar statCard's Anchor tag should be different, it uses commandsearch so never need seesion id, I made seperate StatCard for Calendar. Don Ryu 20250402 */}
-				<a
+				{/* <a
 					href={`/scripts/mwimain.dll/144/WEB_CALENDAR/WEB_CALENDAR_PROFILE?commandsearch&exp=%2B%2B%40&EXP=TAG_FUNC_P_ID%20${m2l_patron_id}&M_GVAR1=USER_ID:${m2l_patron_id}`}
 					className="transition-all duration-300 hover:shadow-lg hover:-translate-y-1 hover:bg-gray-50">
 					<StatCard
@@ -144,11 +144,11 @@ export default function PatronProfile() {
 						color={'pink'}
 						value={records[0].calendar_count}
 					/>
-				</a>
+				</a> */}
 			</div>
 
 			{/* Recent Media Section */}
-			<div className="space-y-4 min-h-[550px]">
+			{/* <div className="space-y-4 min-h-[550px]">
 				<div className="flex items-center justify-between">
 					<h2 className="text-xl font-medium">
 						<span>{message.searchDatabase}</span>
@@ -193,7 +193,7 @@ export default function PatronProfile() {
 						</div>
 					))}
 				</div>
-			</div>
+			</div> */}
 		</PatronLayout>
 	)
 }
