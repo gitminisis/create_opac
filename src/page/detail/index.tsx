b512266600328725a0913ac42b685cd72b74b1e0
mikah13 <anhminhhoang13@gmail.com>
2025-10-15 23:59:41 -0700

    Update document captions in detail page
    Document captions now use the corresponding 'tx_name' value from the record if available, improving clarity over using the media path as the caption.

--- src/page/detail/index.tsx
+++ src/page/detail/index.tsx
@@ -47,9 +47,11 @@ const Detail = () => {
 			],
 		})) || []
 
-	const documents: DocumentProps[] = getMedia(records[0], 'tx_access_link')?.map((e) => ({
+
+
+	const documents: DocumentProps[] = getMedia(records[0], 'tx_access_link')?.map((e,index) => ({
 		src: e.includes('[MEDIA]') ? e.replace('[MEDIA]', '/media/') : e,
-		caption: e.includes('[MEDIA]') ? e.replace('[MEDIA]', '/media/') : e,
+		caption: deepSearchKey(record, 'tx_name')[index] ?? e,
 	})) || []
 	const weekdayToIndex: any = {
 		su: 0,
