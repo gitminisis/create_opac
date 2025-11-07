5a076863e7cb8ce4658d3cffe2327b77c51e44c7
donRyu0125 <don@minisisinc.com>
2025-11-06 17:01:11 -0800

    HOTFIX

--- src/components/common/HighlightText.tsx
+++ src/components/common/HighlightText.tsx
@@ -1,5 +1,5 @@
 export const HighlightText = ({ text, highlights ,className}: { text: string; highlights: string[]; className?:string;}) => {
-	if (!highlights.length) return <p className={`text-foreground text-ellipsis ${className}`}>{text}</p>
+	if (!highlights.length) return <p className={`text-foreground text-ellipsis ${className||''}`}>{text}</p>
 
 	// Create a regex pattern from the highlight words with word boundaries
 	function escapeRegex(str: string) {
@@ -44,7 +44,7 @@ export const HighlightText = ({ text, highlights ,className}: { text: string; hi
 	}
 
 	return (
-		<p className={`text-foreground text-ellipsis ${className}`}>
+		<p className={`text-foreground text-ellipsis ${className||''}`}>
 			{textArray.map((part, index) =>
 				part.isHighlight ? (
 					<span key={index} className="bg-foreground text-background px-1 rounded">
